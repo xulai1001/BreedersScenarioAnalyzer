a40467c3dbe80ec1035b392006e747f8e84c02bc
离披 <lipi@shuise.dev>
2026-03-01 21:29:37 +0800

    添加项目文件。

---
+++ Class1.cs
@@ -0,0 +1,82 @@
+﻿using Gallop;
+using Newtonsoft.Json.Linq;
+using Spectre.Console;
+using System.IO.Compression;
+using UmamusumeResponseAnalyzer;
+using UmamusumeResponseAnalyzer.Plugin;
+
+namespace BreedersScenarioAnalyzer
+{
+    public class BreedersScenarioAnalyzer : IPlugin
+    {
+        public string Name => "梦想杯剧本解析器";
+
+        public string Author => "UmamusumeResponseAnalyzer";
+
+        public string[] Targets => ["Cygames"];
+
+        public async Task UpdatePlugin(ProgressContext ctx)
+        {
+            var progress = ctx.AddTask($"[[{Name}]] 更新");
+
+            using var client = new HttpClient();
+            using var resp = await client.GetAsync($"https://api.github.com/repos/URA-Plugins/{Name}/releases/latest");
+            var json = await resp.Content.ReadAsStringAsync();
+            var jo = JObject.Parse(json);
+
+            var isLatest = ("v" + ((IPlugin)this).Version.ToString()).Equals("v" + jo["tag_name"]?.ToString());
+            if (isLatest)
+            {
+                progress.Increment(progress.MaxValue);
+                progress.StopTask();
+                return;
+            }
+            progress.Increment(25);
+
+            var downloadUrl = jo["assets"][0]["browser_download_url"].ToString().AllowMirror();
+            using var msg = await client.GetAsync(downloadUrl, HttpCompletionOption.ResponseHeadersRead);
+            using var stream = await msg.Content.ReadAsStreamAsync();
+            var buffer = new byte[8192];
+            while (true)
+            {
+                var read = await stream.ReadAsync(buffer);
+                if (read == 0)
+                    break;
+                progress.Increment(read / msg.Content.Headers.ContentLength ?? 1 * 0.5);
+            }
+            using var archive = new ZipArchive(stream);
+            archive.ExtractToDirectory(Path.Combine("Plugins", Name), true);
+            progress.Increment(25);
+
+            progress.StopTask();
+        }
+
+        [Analyzer(priority: 1)]
+        public void Analyzer(JObject jo)
+        {
+            if (!jo.HasCharaInfo()) return;
+            if (jo["data"] is null || jo["data"] is not JObject data) return;
+            if (data["chara_info"] is null || data["chara_info"] is not JObject chara_info) return;
+            if (chara_info["scenario_id"].ToInt() != (int)ScenarioType.Breeders) return;
+            var state = chara_info["state"].ToInt();
+            if (chara_info != null && data["home_info"]?["command_info_array"] != null && data["race_reward_info"].IsNull() && !(state is 2 or 3)) //根据文本简单过滤防止重复、异常输出
+            {
+                var @event = jo.ToObject<Gallop.SingleModeCheckEventResponse>();
+                if ((@event.data.unchecked_event_array != null && @event.data.unchecked_event_array.Length > 0) || @event.data.race_start_info != null) return;
+                Handler.ParseOnsenCommandInfo(@event);
+            }
+        }
+    }
+    public static class BreedersExtensions
+    {
+        extension(SingleModeBreedersTeamMemberInfo charaInfo)
+        {
+            public string Position => charaInfo.member_id switch
+            {
+                1 => "上",
+                2 => "中",
+                3 => "下"
+            };
+        }
+    }
+}
---
+++ Handler.cs
@@ -0,0 +1,355 @@
+﻿using EventLoggerPlugin;
+using Gallop;
+using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UmamusumeResponseAnalyzer;
+using UmamusumeResponseAnalyzer.Game;
+using UmamusumeResponseAnalyzer.Game.TurnInfo;
+using UmamusumeResponseAnalyzer.LocalizedLayout.Handlers;
+using static BreedersScenarioAnalyzer.i18n.Game;
+
+namespace BreedersScenarioAnalyzer
+{
+    public static class Handler
+    {
+        public static void ParseOnsenCommandInfo(SingleModeCheckEventResponse @event)
+        {
+            var stage = @event.GetCommandInfoStage();
+            var layout = new Layout().SplitColumns(
+                new Layout("Main").Size(CommandInfoLayout.Current.MainSectionWidth).SplitRows(
+                    new Layout("体力干劲条").SplitColumns(
+                        new Layout("日期").Ratio(4),
+                        new Layout("总属性").Ratio(6),
+                        new Layout("体力").Ratio(6),
+                        new Layout("干劲").Ratio(3)).Size(3),
+                    new Layout("重要信息").Size(5),
+                    new Layout("剧本信息").SplitColumns(
+                        new Layout("温泉券").Ratio(1),
+                        new Layout("温泉Buff").Ratio(1),
+                        new Layout("特殊训练").Ratio(1),
+                        new Layout("挖掘进度").Ratio(1)
+                        ).Size(3),
+                    //new Layout("分割", new Rule()).Size(1),
+                    new Layout("训练信息")  // size 20, 共约30行
+                    ).Ratio(4),
+                new Layout("Ext").Ratio(1)
+                );
+            var noTrainingTable = false;
+            var critInfos = new List<string>();
+            var turn = new TurnInfoBreeders(@event.data);
+            layout["特殊训练"].Update(new Panel($"{string.Join(string.Empty, Enumerable.Repeat("●", turn.SpecialTrainingStock))}{(turn.SpecialTraningActivated ? "[green]●[/]" : string.Empty)}{string.Join(string.Empty, Enumerable.Repeat("○", turn.SpecialTrainingMax - turn.SpecialTrainingStock - (turn.SpecialTraningActivated ? 1 : 0)))}").Expand());
+            var dataset = @event.data.breeders_data_set;
+
+            if (GameStats.currentTurn != turn.Turn - 1 //正常情况
+                && GameStats.currentTurn != turn.Turn //重复显示
+                && turn.Turn != 1 //第一个回合
+                )
+            {
+                GameStats.isFullGame = false;
+                critInfos.Add(string.Format(I18N_WrongTurnAlert, GameStats.currentTurn, turn.Turn));
+                EventLogger.Init(@event);
+                EventLogger.IsStart = true;
+            }
+            else if (turn.Turn == 1)
+            {
+                GameStats.isFullGame = true;
+                EventLogger.Init(@event);
+                EventLogger.IsStart = true;
+            }
+
+            //买技能，大师杯剧本年末比赛，会重复显示
+            if (@event.data.chara_info.playing_state != 1)
+            {
+                critInfos.Add(I18N_RepeatTurn);
+            }
+            else
+            {
+                //初始化TurnStats
+                GameStats.whichScenario = @event.data.chara_info.scenario_id;
+                GameStats.currentTurn = turn.Turn;
+                GameStats.stats[turn.Turn] = new TurnStats();
+                EventLogger.Update(@event);
+            }
+            // T3 在EventLogger更新后需要开始捕获体力消耗
+            if (turn.Turn == 3)
+            {
+                EventLogger.captureVitalSpending = true;
+            }
+            var trainItems = new Dictionary<int, SingleModeCommandInfo>
+            {
+                { 101, @event.data.home_info.command_info_array[0] },
+                { 105, @event.data.home_info.command_info_array[1] },
+                { 102, @event.data.home_info.command_info_array[2] },
+                { 103, @event.data.home_info.command_info_array[3] },
+                { 106, @event.data.home_info.command_info_array[4] }
+            };
+            var trainStats = new TrainStats[5];
+            var turnStat = @event.data.chara_info.playing_state != 1 ? new TurnStats() : GameStats.stats[turn.Turn];
+            turnStat.motivation = @event.data.chara_info.motivation;
+            var failureRate = new Dictionary<int, int>();
+
+            // 总属性计算
+            var currentFiveValue = new int[]
+            {
+                @event.data.chara_info.speed,
+                @event.data.chara_info.stamina,
+                @event.data.chara_info.power ,
+                @event.data.chara_info.guts ,
+                @event.data.chara_info.wiz ,
+            };
+            var fiveValueMaxRevised = new int[]
+            {
+                ScoreUtils.ReviseOver1200(@event.data.chara_info.max_speed),
+                ScoreUtils.ReviseOver1200(@event.data.chara_info.max_stamina),
+                ScoreUtils.ReviseOver1200(@event.data.chara_info.max_power) ,
+                ScoreUtils.ReviseOver1200(@event.data.chara_info.max_guts) ,
+                ScoreUtils.ReviseOver1200(@event.data.chara_info.max_wiz) ,
+            };
+            var currentFiveValueRevised = currentFiveValue.Select(ScoreUtils.ReviseOver1200).ToArray();
+            var totalValue = currentFiveValueRevised.Sum();
+            var totalValueWithPt = totalValue + @event.data.chara_info.skill_point;
+
+            for (var i = 0; i < 5; i++)
+            {
+                var trainId = TurnInfoBreeders.TrainIds[i];
+                failureRate[trainId] = trainItems[trainId].failure_rate;
+                var trainParams = new Dictionary<int, int>()
+                {
+                    {1,0},
+                    {2,0},
+                    {3,0},
+                    {4,0},
+                    {5,0},
+                    {30,0},
+                    {10,0},
+                };
+                foreach (var item in turn.GetCommonResponse().home_info.command_info_array)
+                {
+                    if (TurnInfoBreeders.ToTrainId.TryGetValue(item.command_id, out var value) && value == trainId)
+                    {
+                        foreach (var trainParam in item.params_inc_dec_info_array)
+                            trainParams[trainParam.target_type] += trainParam.value;
+                    }
+                }
+
+                var stats = new TrainStats
+                {
+                    FailureRate = trainItems[trainId].failure_rate,
+                    VitalGain = trainParams[10]
+                };
+                if (turn.Vital + stats.VitalGain > turn.MaxVital)
+                    stats.VitalGain = turn.MaxVital - turn.Vital;
+                if (stats.VitalGain < -turn.Vital)
+                    stats.VitalGain = -turn.Vital;
+                stats.FiveValueGain = [trainParams[1], trainParams[2], trainParams[3], trainParams[4], trainParams[5]];
+                stats.PtGain = trainParams[30];
+
+                var valueGainUpper = dataset.command_info_array.FirstOrDefault(x => x.command_id == trainId || x.command_id == TurnInfoBreeders.XiahesuIds[trainId])?.params_inc_dec_info_array;
+                if (valueGainUpper != null)
+                {
+                    foreach (var item in valueGainUpper)
+                    {
+                        if (item.target_type == 30)
+                            stats.PtGain += item.value;
+                        else if (item.target_type <= 5)
+                            stats.FiveValueGain[item.target_type - 1] += item.value;
+                    }
+                }
+
+                for (var j = 0; j < 5; j++)
+                    stats.FiveValueGain[j] = ScoreUtils.ReviseOver1200(turn.Stats[j] + stats.FiveValueGain[j]) - ScoreUtils.ReviseOver1200(turn.Stats[j]);
+
+                if (turn.Turn == 1)
+                {
+                    turnStat.trainLevel[i] = 1;
+                    turnStat.trainLevelCount[i] = 0;
+                }
+                else
+                {
+                    var lastTrainLevel = GameStats.stats[turn.Turn - 1] != null ? GameStats.stats[turn.Turn - 1].trainLevel[i] : 1;
+                    var lastTrainLevelCount = GameStats.stats[turn.Turn - 1] != null ? GameStats.stats[turn.Turn - 1].trainLevelCount[i] : 0;
+
+                    turnStat.trainLevel[i] = lastTrainLevel;
+                    turnStat.trainLevelCount[i] = lastTrainLevelCount;
+                    if (GameStats.stats[turn.Turn - 1] != null &&
+                        GameStats.stats[turn.Turn - 1].playerChoice == TurnInfoBreeders.TrainIds[i] &&
+                        !GameStats.stats[turn.Turn - 1].isTrainingFailed &&
+                        !((turn.Turn - 1 >= 37 && turn.Turn - 1 <= 40) || (turn.Turn - 1 >= 61 && turn.Turn - 1 <= 64))
+                        )//上回合点的这个训练，计数+1
+                        turnStat.trainLevelCount[i] += 1;
+                    if (turnStat.trainLevelCount[i] >= 4)
+                    {
+                        turnStat.trainLevelCount[i] -= 4;
+                        turnStat.trainLevel[i] += 1;
+                    }
+                    //检查是否有剧本全体训练等级+1
+                    if (turn.Turn == 25 || turn.Turn == 37 || turn.Turn == 49)
+                        turnStat.trainLevelCount[i] += 4;
+                    if (turnStat.trainLevelCount[i] >= 4)
+                    {
+                        turnStat.trainLevelCount[i] -= 4;
+                        turnStat.trainLevel[i] += 1;
+                    }
+
+                    if (turnStat.trainLevel[i] >= 5)
+                    {
+                        turnStat.trainLevel[i] = 5;
+                        turnStat.trainLevelCount[i] = 0;
+                    }
+
+                    var trainlv = @event.data.chara_info.training_level_info_array.First(x => x.command_id == TurnInfoBreeders.TrainIds[i]).level;
+                    if (turnStat.trainLevel[i] != trainlv && stage == 2)
+                    {
+                        //可能是半途开启小黑板，也可能是有未知bug
+                        critInfos.Add($"[red]警告：训练等级预测错误，预测{TurnInfoBreeders.TrainIds[i]}为lv{turnStat.trainLevel[i]}(+{turnStat.trainLevelCount[i]})，实际为lv{trainlv}[/]");
+                        turnStat.trainLevel[i] = trainlv;
+                        turnStat.trainLevelCount[i] = 0;//如果是半途开启小黑板，则会在下一次升级时变成正确的计数
+                    }
+                }
+
+                trainStats[i] = stats;
+            }
+            if (stage == 2)
+            {
+                // 把训练等级信息更新到GameStats
+                turnStat.fiveTrainStats = trainStats;
+                GameStats.stats[turn.Turn] = turnStat;
+            }
+
+            //训练或比赛阶段
+            if (stage == 2)
+            {
+                var grids = new Grid();
+                grids.AddColumns(6);
+                foreach (var column in grids.Columns)
+                {
+                    column.Padding = new Padding(0, 0, 0, 0);
+                }
+
+                var failureRateStr = new string[5];
+                //失败率>=40%标红、>=20%(有可能大失败)标DarkOrange、>0%标黄
+                for (var i = 0; i < 5; i++)
+                {
+                    var thisFailureRate = failureRate[TurnInfoBreeders.TrainIds[i]];
+                    failureRateStr[i] = thisFailureRate switch
+                    {
+                        >= 40 => $"[red]({thisFailureRate}%)[/]",
+                        >= 20 => $"[darkorange]({thisFailureRate}%)[/]",
+                        > 0 => $"[yellow]({thisFailureRate}%)[/]",
+                        _ => string.Empty
+                    };
+                }
+
+                var commands = turn.CommandInfoArray.Select(command =>
+                {
+                    var table = new Table()
+                    .AddColumn(command.TrainIndex switch
+                    {
+                        1 => $"{I18N_Speed}{failureRateStr[0]}",
+                        2 => $"{I18N_Stamina}{failureRateStr[1]}",
+                        3 => $"{I18N_Power}{failureRateStr[2]}",
+                        4 => $"{I18N_Nuts}{failureRateStr[3]}",
+                        5 => $"{I18N_Wiz}{failureRateStr[4]}"
+                    });
+
+                    var currentStat = turn.StatsRevised[command.TrainIndex - 1];
+                    var statUpToMax = turn.MaxStatsRevised[command.TrainIndex - 1] - currentStat;
+                    table.AddRow(I18N_CurrentRemainStat);
+                    table.AddRow($"{currentStat}:{statUpToMax switch
+                    {
+                        > 400 => $"{statUpToMax}",
+                        > 200 => $"[yellow]{statUpToMax}[/]",
+                        _ => $"[red]{statUpToMax}[/]"
+                    }}");
+                    table.AddRow(new Rule());
+
+                    var members = turn.CommandTeamMemmberInfoDictionary[command.CommandId].Select(x => turn.TeamMemberInfoDictionary[x.chara_id]);
+
+                    table.AddRow($"Lv{command.TrainLevel} | {string.Join(',', members.Select(x => x.Position))}");
+                    table.AddRow(new Rule());
+
+                    var stats = trainStats[command.TrainIndex - 1];
+                    var score = stats.FiveValueGain.Sum();
+                    if (score == trainStats.Max(x => x.FiveValueGain.Sum()))
+                        table.AddRow($"{I18N_StatSimple}:[aqua]{score}[/]|Pt:{stats.PtGain}");
+                    else
+                        table.AddRow($"{I18N_StatSimple}:{score}|Pt:{stats.PtGain}");
+
+                    foreach (var trainingPartner in command.TrainingPartners)
+                    {
+                        table.AddRow(trainingPartner.Name);
+                        if (trainingPartner.Shining)
+                            table.BorderColor(Color.LightGreen);
+                    }
+                    for (var i = 5 - command.TrainingPartners.Count(); i > 0; i--)
+                    {
+                        table.AddRow(string.Empty);
+                    }
+                    table.AddRow(new Rule());
+
+                    return new Padder(table).Padding(0, 0, 0, 0);
+                }).ToList();
+                grids.AddRow([.. commands]);
+
+                layout["训练信息"].Update(grids);
+            }
+            else
+            {
+                var grids = new Grid();
+                grids.AddColumns(1);
+                grids.AddRow([$"非训练阶段，stage={stage}"]);
+                layout["训练信息"].Update(grids);
+                noTrainingTable = true;
+            }
+
+            // 额外信息
+            var exTable = new Table().AddColumn("Extras");
+            exTable.HideHeaders();
+            // 计算连续事件表现
+            var eventPerf = EventLogger.PrintCardEventPerf(@event.data.chara_info.scenario_id);
+            if (eventPerf.Count > 0)
+            {
+                exTable.AddRow(new Rule());
+                foreach (var row in eventPerf)
+                    exTable.AddRow(new Markup(row));
+            }
+
+            layout["日期"].Update(new Panel($"{turn.Year}{I18N_Year} {turn.Month}{I18N_Month}{turn.HalfMonth}").Expand());
+            layout["总属性"].Update(new Panel($"[cyan]总属性: {totalValue}, Pt: {@event.data.chara_info.skill_point}[/]").Expand());
+            layout["体力"].Update(new Panel($"{I18N_Vital}: [green]{turn.Vital}[/]/{turn.MaxVital}").Expand());
+            layout["干劲"].Update(new Panel(@event.data.chara_info.motivation switch
+            {
+                // 换行分裂和箭头符号有关，去掉
+                5 => $"[green]{I18N_MotivationBest}[/]",
+                4 => $"[yellow]{I18N_MotivationGood}[/]",
+                3 => $"[red]{I18N_MotivationNormal}[/]",
+                2 => $"[red]{I18N_MotivationBad}[/]",
+                1 => $"[red]{I18N_MotivationWorst}[/]"
+            }).Expand());
+
+            var availableTrainingCount = @event.data.home_info.command_info_array.Count(x => x.is_enable == 1);
+            if (availableTrainingCount <= 1)
+            {
+                critInfos.Add("[aqua]非训练回合[/]");
+            }
+            if (@event.data.chara_info.skill_point > 9500)
+            {
+                critInfos.Add("[red]剩余PT>9500（上限9999），请及时学习技能");
+            }
+            layout["重要信息"].Update(new Panel(string.Join(Environment.NewLine, critInfos)).Expand());
+
+            layout["Ext"].Update(exTable);
+
+            GameStats.Print();
+
+            AnsiConsole.Write(layout);
+            // 光标倒转一点
+            if (noTrainingTable)
+                AnsiConsole.Cursor.SetPosition(0, 15);
+            else
+                AnsiConsole.Cursor.SetPosition(0, 31);
+        }
+    }
+}
